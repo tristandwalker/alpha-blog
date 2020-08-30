748e86ae4375e884900e293a3670cad95117a754
Tristan Walker <tristandieselwalker@gmail.com>
2020-08-30 09:26:38 -0600

    add articles listing feature on articles index page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,11 @@
 class ArticlesController < ApplicationController
 
     def show
-        byebug
         @article = Article.find(params[:id])
+    end
+
+    def index
+        @article = Article.all
     end
     
 end
